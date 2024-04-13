289c4b99dea99aa4b1065a1a3c7663d0eb7ee056
Maaz Husyan <159529283+MaazHusyan@users.noreply.github.com>
2024-04-13 11:59:45 +0500

    Typescript cli ATM Machine

---
+++ index.js
@@ -0,0 +1,69 @@
+import inquirer from "inquirer";
+let myBalance = 100000;
+let myPin = 1;
+let selectPin = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your pin",
+        type: "number",
+    },
+]);
+if (selectPin.pin === myPin) {
+    console.log("Correct pin code");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Select your operation",
+            type: "list",
+            choices: ["Fast Cash", "Withdraw", "Balance Check"],
+        },
+    ]);
+    if (operationAns.operation === "Fast Cash") {
+        let operationFastCash = await inquirer.prompt([
+            {
+                name: "fastCash",
+                message: "Select your Amount",
+                type: "list",
+                choices: [500, 1000, 5000, 10000]
+            }
+        ]);
+        if (operationFastCash.fastCash === 500) {
+            myBalance -= operationFastCash.fastCash;
+            console.log(`Your Current Balance in $${myBalance}`);
+        }
+        else if (operationFastCash.fastCash === 1000) {
+            myBalance -= operationFastCash.fastCash;
+            console.log(`Your Current Balance in $${myBalance}`);
+        }
+        else if (operationFastCash.fastCash === 5000) {
+            myBalance -= operationFastCash.fastCash;
+            console.log(`Your Current Balance in $${myBalance}`);
+        }
+        else if (operationFastCash.fastCash === 10000) {
+            myBalance -= operationFastCash.fastCash;
+            console.log(`Your Current Balance in $${myBalance}`);
+        }
+    }
+    if (operationAns.operation === "Withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Select your Amount",
+                type: "number",
+            },
+        ]);
+        if (amountAns.amount <= myBalance) {
+            myBalance -= amountAns.amount;
+            console.log(`Your remaining Balance is $${myBalance} `);
+        }
+        else {
+            console.log("Not enough money");
+        }
+    }
+    else if (operationAns.operation === "Balance Check") {
+        console.log(`Your Current Balance is $${myBalance} `);
+    }
+}
+else {
+    console.log("Incorrect pin code");
+}
